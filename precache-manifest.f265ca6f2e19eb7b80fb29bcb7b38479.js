d16fa1db02388a79b09a98d4021d3a636ef2c299
axie66 <alex.xie7734@gmail.com>
2020-12-23 18:49:48 -0500

    Updates

---
+++ precache-manifest.f265ca6f2e19eb7b80fb29bcb7b38479.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380a46062748eef75d4787a64d9c0b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eeef6cac59d936beb56",
+    "url": "/static/css/main.fb3affb4.chunk.css"
+  },
+  {
+    "revision": "032cd266ffda6bbc5420",
+    "url": "/static/js/2.7c044d71.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7c044d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eeef6cac59d936beb56",
+    "url": "/static/js/main.60b60b59.chunk.js"
+  },
+  {
+    "revision": "0413dca6c447f48db67e",
+    "url": "/static/js/runtime-main.0f6914af.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "52eb73dbce8bf4083a59acc2e91699ad",
+    "url": "/static/media/SpaceMono-Regular.52eb73db.ttf"
+  }
+]);
